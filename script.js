e49e5abea6c0a330ce1638b74ea6113fbd48c24e
George Chinaka <132566100+georgestcloud@users.noreply.github.com>
2024-05-12 13:38:05 +0100

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     setTimeout(function() {
-      var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      if (screenWidth < 800) {
-        document.getElementById("heading").style.display = "none";
-      }
+        var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+        if (screenWidth < 800) {
+            document.getElementById("heading").style.display = "none";
+            document.getElementById("image").src = "lion.gif"; // Change image source to "lion.gif"
+        }
     }, 7000); // 7 seconds
-  });
-  
+});
 
 document.addEventListener("DOMContentLoaded", function() {
     // Hide the h1 after 10 seconds
